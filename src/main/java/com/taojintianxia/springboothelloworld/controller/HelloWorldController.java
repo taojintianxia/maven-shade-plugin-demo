f607924e822c2fd41ef8b56b3f5977272ee59b01
sunnianjun <sunnianjun@huobi.com>
2018-12-12 15:42:50 +0800

    Add: 添加hello world

---
+++ src/main/java/com/taojintianxia/springboothelloworld/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.taojintianxia.springboothelloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Nianjun Sun
+ * @date 2018/12/12 15:40
+ */
+
+@RestController()
+public class HelloWorldController {
+
+    @GetMapping("hello")
+    public String helloWorld() {
+        System.out.println("Hello World");
+        return "Hello, World !";
+    }
+
+}
