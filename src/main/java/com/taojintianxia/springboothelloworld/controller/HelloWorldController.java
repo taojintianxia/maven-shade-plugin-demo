aa4b379fca112d39a18f53de920bb2d08ec0e22e
sunnianjun <sunnianjun@huobi.com>
2019-01-17 13:58:44 +0800

    Add: add a test method, later will move to incubator project

--- src/main/java/com/taojintianxia/springboothelloworld/controller/HelloWorldController.java
+++ src/main/java/com/taojintianxia/springboothelloworld/controller/HelloWorldController.java
@@ -1,7 +1,13 @@
 package com.taojintianxia.springboothelloworld.controller;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author Nianjun Sun
@@ -17,4 +23,18 @@ public class HelloWorldController {
         return "Hello, World !";
     }
 
+    @GetMapping("test")
+    public void test() throws IOException {
+        System.out.println("start to print file inside jar");
+        Resource bashFileResource = new ClassPathResource("templates/run.sh");
+        InputStream inputStream = bashFileResource.getInputStream();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] cache = new byte[512];
+        while (inputStream.read(cache) > -1) {
+            byteArrayOutputStream.write(cache);
+        }
+
+        System.out.println(byteArrayOutputStream.toString());
+    }
 }
+
