66f3ffe9f6d2170e7fc2e44bf1a0eccae01af6c5
sunnianjun <sunnianjun@huobi.com>
2019-04-27 15:43:53 +0800

    Add: 添加 mr 测试

--- src/main/java/com/taojintianxia/springboothelloworld/controller/HelloWorldController.java
+++ src/main/java/com/taojintianxia/springboothelloworld/controller/HelloWorldController.java
@@ -14,7 +14,7 @@ public class HelloWorldController {
     @GetMapping("hello")
     public String helloWorld() {
         System.out.println("Hello World");
-        return "Hello, World!";
+        return "Hello, World! Mr ";
     }
 }
 
