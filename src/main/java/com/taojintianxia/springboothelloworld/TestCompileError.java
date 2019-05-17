a17d2b846162515671057b06a8fa285eedb0e09c
sunnianjun <sunnianjun@huobi.com>
2019-05-17 15:41:12 +0800

    Add: keep on testing compile error

--- src/main/java/com/taojintianxia/springboothelloworld/TestCompileError.java
+++ src/main/java/com/taojintianxia/springboothelloworld/TestCompileError.java
@@ -7,6 +7,6 @@ package com.taojintianxia.springboothelloworld;
 public class TestCompileError {
 
     public static void main(String...args){
-        System.out.println('');
+        System.out.println(');
     }
 }
