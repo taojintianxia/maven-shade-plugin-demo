f0691647090f5a9c60961bdee141b3f3d872a3c0
sunnianjun <sunnianjun@huobi.com>
2019-05-17 15:36:02 +0800

    Add: add a test code to test compile error

---
+++ src/main/java/com/taojintianxia/springboothelloworld/TestCompileError.java
@@ -0,0 +1,12 @@
+package com.taojintianxia.springboothelloworld;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019-05-17 15:35
+ */
+public class TestCompileError {
+
+    public static void main(String...args){
+        System.out.println('');
+    }
+}
